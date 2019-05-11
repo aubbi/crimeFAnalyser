3f42768824f2fd8178872e48218fc9d4fe62649b
Adel <ea_gasmi@esi.dz>
2019-05-12 00:50:37 +0100

    Modifying PlotlyJs file

--- static/forecasting/js/plotlygraph.js
+++ static/forecasting/js/plotlygraph.js
@@ -32,26 +32,44 @@ function forecastAll(records) {
     } */
 
   for (i = 0; i < Object.size(records.crimes); i++) {
-    price = records.crimes[i].y;
-    x1.push(new Date(records.crimes[i].ds));
-    x1.push(i);
-    y1.push(price);
+    number = records.crimes[i].y;
+    date = records.crimes[i].ds;
+    x1.push(date);
+    y1.push(number);
   }
-
-  console.log(x1);
   var trace1 = {
     x: x1,
     y: y1,
+    mode: "lines",
     type: "scatter"
   };
 
+  for (i = 0; i < Object.size(records.forecast); i++) {
+    number2 = records.forecast[i].yhat;
+    date2 = records.forecast[i].ds;
+    x1.push(date2);
+    y1.push(number2);
+  }
+
   var trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
+    x: x2,
+    y: y2,
+    mode: "lines+markers",
     type: "scatter"
   };
 
   var data = [trace1, trace2];
 
-  Plotly.newPlot("myDiv", data, {}, { showSendToCloud: true });
+  var layout = {
+    xaxis: {
+      type: "date",
+      title: "January Weather"
+    },
+    yaxis: {
+      title: "Daily Mean Temperature"
+    },
+    title: "2000 Toronto January Weather"
+  };
+
+  Plotly.newPlot("myDiv", data, layout, { showSendToCloud: true });
 }
