b79e5b2d6c2127c030389102b4835eac4d8fac24
Adel <ea_gasmi@esi.dz>
2019-05-23 14:40:38 +0100

    Code refactoring

--- predict/urls.py
+++ predict/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('predict_prophet/', views.predictCrimes, name='showForecast'),
     url(r'^makeForecast', views.makeForecast, name="makeForecast"),
-    path('other_forecasts/', views.makeOtherForecasts, name='makeOtherForecasts'),
+    path('instructions/', views.makeForecast, name='instructions'),
     url(r'^makeOtherForecast', views.makeOtherForecasts, name="makeOtherForecasts"),
     path('predict_period/', views.predictCrimes, name='predict'),
     path('predict_region/', views.predictCrimes, name='predict'),
