9ee057207e03423173287252bac9db3d24883e1a
Ayyoub GRINA <grinaay@gmail.com>
2019-06-23 19:21:38 +0100

    added settings in user model

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+#Store users settings here
 class Settings(models.Model):
     user = models.OneToOneField(User, on_delete="models.CASCADE")
     limit = models.IntegerField(default=20000)
