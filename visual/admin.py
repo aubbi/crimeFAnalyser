97f767504781acdc6a0096ee36595de48b46e9e7
Ayyoub GRINA <grinaay@gmail.com>
2019-06-23 18:53:15 +0100

    fixed district clustering map added settings

--- visual/admin.py
+++ visual/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+
 # Register your models here.
 
 from .models import Crime
+from users.models import Settings
 
 admin.site.site_header = 'Administration du système'
 
 admin.site.register(Crime)
+admin.site.register(Settings)
 
 
 class CrimeAdmin (admin.ModelAdmin):
